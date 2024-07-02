47cac651ae38bd58a71581daad4b4bc46b1023ac
NhatUET <minhnhat8dc@gmail.com>
2024-07-02 22:08:58 +0700

    add structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='WasteDetection',
+    version='0.0.0',
+    author='VN',
+    author_email='minhnhat8dc@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
